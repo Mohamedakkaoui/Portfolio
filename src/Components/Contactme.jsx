8a23c1693aa72dbbac50a3862912c8f5dc1d2c55
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-07-10 23:53:20 +0100

    Styled the contact by adding a background image

--- src/Components/Contactme.jsx
+++ src/Components/Contactme.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import MapImg from '../assets/map.png';
 
 function Contactme() {
   return (
-    <div className="bg-[#161617]">
-      <div className="flex flex-col  min-h-screen w-[90%] mx-auto py-[120px]">
+    <div className="relative bg-[#161617] min-h-screen">
+      <div
+        className="absolute inset-0"
+        style={{
+          backgroundImage: `url(${MapImg})`,
+          backgroundRepeat: 'repeat',
+          backgroundSize: '50%',
+          backgroundPosition: 'center'
+        }}
+      ></div>
+      <div className="relative flex flex-col min-h-screen w-[90%] mx-auto py-[120px] bg-opacity-75 ">
         <div className="mb-19">
           <h1 className="text-white text-center font-bold text-8xl tracking-wide mb-[90px]">
             Contact Me
           </h1>
         </div>
         <div className="flex text-white gap-8">
-          <div className="w-[35%] ">
+          <div className="w-[35%]">
             <div className="text-[#c49b66] font-semibold mb-1 tracking-widest">
               PHONE :
             </div>
@@ -31,19 +41,18 @@ function Contactme() {
           <div className="w-[60%]">
             <div className="flex justify-between mb-4">
               <div className="w-[45%]">
-                {" "}
-                <div class="relative mt-1">
+                <div className="relative mt-1">
                   <input
                     type="text"
                     id="Full Name"
-                    className="block w-full p-3 ps-6 text-[#9ca3af] focus:outline-none rounded-full bg-[#222222] "
+                    className="block w-full p-3 ps-6 text-[#9ca3af] focus:outline-none rounded-full bg-[#222222]"
                     placeholder="Name"
                     required
                   />
                 </div>
               </div>
               <div className="w-[45%]">
-                <div class="relative mt-1">
+                <div className="relative mt-1">
                   <input
                     type="email"
                     id="Full Name"
@@ -55,12 +64,11 @@ function Contactme() {
               </div>
             </div>
             <div>
-              {" "}
-              <div class="relative mt-1 outline-none">
+              <div className="relative mt-1 outline-none">
                 <input
                   type="text"
                   id="Full Name"
-                  className="block w-full p-3 ps-6 text-[#9ca3af] focus:outline-none rounded-full bg-[#222222] "
+                  className="block w-full p-3 ps-6 text-[#9ca3af] focus:outline-none rounded-full bg-[#222222]"
                   placeholder="Subject"
                   required
                 />
@@ -71,7 +79,7 @@ function Contactme() {
                 name="Message"
                 placeholder="Message"
                 id="Message"
-                className=" text-[#9ca3af] focus:outline-none p-3 ps-6 w-full h-[150px] mt-4 rounded-xl bg-[#222222] "
+                className="text-[#9ca3af] focus:outline-none p-3 ps-6 w-full h-[150px] mt-4 rounded-xl bg-[#222222]"
               ></textarea>
             </div>
           </div>
