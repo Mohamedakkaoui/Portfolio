78cf78142fb27c7f8952796cbb1f948a76d6b4bd
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-07-14 23:14:05 +0100

    Added some rotation effect for the links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Github, Linkedin, Twitter, Instagram, Facebook } from "lucide-react";
 
 function Footer() {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIndex(null);
+  };
+
   return (
     <>
       <div className="bg-[#0f0f0f] pt-20 pb-10">
@@ -9,46 +19,54 @@ function Footer() {
           Akkaoui
         </div>
         <div className="flex text-[#c49b66] items-center justify-center gap-8 pt-8">
-          <div
-            className="hover:cursor-pointer p-5 bg-[#222] rounded-xl "
-            style={{ transform: "rotate(45deg)" }}
-          >
-            <a href="https://github.com/Mohamedakkaoui">
-              <Github size={18} style={{ transform: "rotate(-45deg)" }} />
-            </a>
-          </div>
-          <div
-            className="hover:cursor-pointer p-5 bg-[#222] rounded-xl "
-            style={{ transform: "rotate(45deg)" }}
-          >
-            <a href="https://www.linkedin.com/in/mohamedakkaoui/">
-              <Linkedin size={18} style={{ transform: "rotate(-45deg)" }} />
-            </a>
-          </div>
-          <div
-            className="hover:cursor-pointer p-5 bg-[#222] rounded-xl"
-            style={{ transform: "rotate(45deg)" }}
-          >
-            <a href="https://x.com/_datboiiMohamed">
-              <Twitter size={18} style={{ transform: "rotate(-45deg)" }} />
-            </a>
-          </div>
-          <div
-            className="hover:cursor-pointer p-5 bg-[#222] rounded-xl"
-            style={{ transform: "rotate(45deg)" }}
-          >
-            <a href="https://www.instagram.com/mohamed_akkaoui/">
-              <Instagram size={18} style={{ transform: "rotate(-45deg)" }} />
-            </a>
-          </div>
-          <div
-            className="hover:cursor-pointer p-5 bg-[#222] rounded-xl"
-            style={{ transform: "rotate(45deg)" }}
-          >
-            <a href="https://web.facebook.com/profile.php?id=100008860692186">
-              <Facebook size={18} style={{ transform: "rotate(-45deg)" }} />
-            </a>
-          </div>
+          {[
+            {
+              href: "https://github.com/Mohamedakkaoui",
+              icon: <Github size={18} />,
+            },
+            {
+              href: "https://www.linkedin.com/in/mohamedakkaoui/",
+              icon: <Linkedin size={18} />,
+            },
+            {
+              href: "https://x.com/_datboiiMohamed",
+              icon: <Twitter size={18} />,
+            },
+            {
+              href: "https://www.instagram.com/mohamed_akkaoui/",
+              icon: <Instagram size={18} />,
+            },
+            {
+              href: "https://web.facebook.com/profile.php?id=100008860692186",
+              icon: <Facebook size={18} />,
+            },
+          ].map((link, index) => (
+            <div
+              key={index}
+              className="hover:cursor-pointer p-5 bg-[#222] rounded-xl hover:bg-[#c49b66] hover:text-white"
+              style={{
+                transform:
+                  hoveredIndex === index ? "rotate(0deg)" : "rotate(45deg)",
+                transition: "transform 0.3s",
+              }}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeave()}
+            >
+              <a href={link.href}>
+                <div
+                  style={{
+                    transform:
+                      hoveredIndex === index
+                        ? "rotate(0deg)"
+                        : "rotate(-45deg)",
+                    transition: "transform 0.3s",
+                  }}
+                >
+                  {link.icon}
+                </div>
+              </a>
+            </div>
+          ))}
         </div>
         <div className="text-[#999999] text-center mt-7">
           © 2024 UI-Depecez-vous. ALL RIGHTS RESERVED.
