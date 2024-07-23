ee189917bbb1647d31cdab515b40461176ac94a9
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-07-23 22:22:28 +0100

    Intial Layout for the Projects section

--- Client/src/Components/Portfolio.jsx
+++ Client/src/Components/Portfolio.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function Projects() {
   return (
     <>
-
+      <div className="bg-[#161617] flex flex-col pb-10">
+        <div className="mb-19">
+          <h1 className="text-white text-center font-bold text-8xl tracking-wide mb-[90px]">
+            Selected Work
+          </h1>
+        </div>
+        <div className="flex flex-col">
+          
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
