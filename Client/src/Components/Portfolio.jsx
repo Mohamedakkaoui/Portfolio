5aea3c0a9f2d68bbb7e1524eba0af32a73cd44bb
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-08-04 23:29:04 +0100

    skeleton for the protfolio section

--- Client/src/Components/Portfolio.jsx
+++ Client/src/Components/Portfolio.jsx
@@ -3,14 +3,19 @@ import React from "react";
 function Projects() {
   return (
     <>
-      <div className="bg-[#161617] flex flex-col pb-10">
+      <div className="bg-[#161617] flex flex-col pb-10 min-h-screen">
         <div className="mb-19">
           <h1 className="text-white text-center font-bold text-8xl tracking-wide mb-[90px]">
             Selected Work
           </h1>
         </div>
-        <div className="flex flex-col">
-          
+        <div className="flex">
+          <div className="flex flex-col">
+            <div>Real estate website</div>
+            <div>Agency website</div>
+          </div>
+          <div>E-learning platform</div>
+          <div>Bookstore Api</div>
         </div>
       </div>
     </>
