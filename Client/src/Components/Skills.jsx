df2431ccf274674bd857b32e1f15cfd26f7846e7
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-08-11 17:32:18 +0100

    Skiils Section

--- Client/src/Components/Skills.jsx
+++ Client/src/Components/Skills.jsx
@@ -14,32 +14,45 @@ import ReactJs from "../assets/icons8-react-50.png";
 import Tailwindcss from "../assets/icons8-tailwind-css-48.png";
 function Skills() {
   const Icons = [
-    { id: 1, icon: NodeJs },
-    { id: 2, icon: ExpressJs },
-    { id: 3, icon: Javascript },
-    { id: 4, icon: Python },
-    { id: 5, icon: C },
-    { id: 6, icon: Css },
-    { id: 7, icon: Html },
-    { id: 8, icon: MongoDb },
-    { id: 9, icon: Typescript },
-    { id: 10, icon: Git },
-    { id: 11, icon: Github },
-    { id: 12, icon: ReactJs },
-    { id: 13, icon: Tailwindcss },
+    { id: 1, icon: NodeJs, text: "Node.js" },
+    { id: 2, icon: ExpressJs, text: "Express.js" },
+    { id: 3, icon: Javascript, text: "Javascript" },
+    { id: 4, icon: Python, text: "Python" },
+    { id: 5, icon: C, text: "C" },
+    { id: 6, icon: Css, text: "CSS" },
+    { id: 7, icon: Html, text: "HTML" },
+    { id: 8, icon: MongoDb, text: "MongoDB" },
+    { id: 9, icon: Typescript, text: "Typescript" },
+    { id: 10, icon: Git, text: "Git" },
+    { id: 11, icon: Github, text: "Github" },
+    { id: 12, icon: ReactJs, text: "React.js" },
+    { id: 13, icon: Tailwindcss, text: "Tailwind.css" },
   ];
   return (
-    <div className="relative bg-[#161617] min-h-screen text-white">
-      <div className="relative flex flex-col min-h-screen w-[90%] mx-auto pb-[120px] pt-[60px] bg-opacity-75 ">
+    <div className="relative bg-[#161617] min-h-screen text-white ">
+      <div className="relative flex flex-col min-h-screen w-[80%] mx-auto pb-[120px] pt-[60px] bg-opacity-75 ">
         <div className="mb-19">
           <h1 className="text-white text-center font-bold text-8xl tracking-wide mb-[90px]">
             Skills
           </h1>
         </div>
-        <div className="flex flex-wrap gap-12 justify-center ">
+        <div className="flex flex-wrap space-x-16 justify-center items-center">
           {Icons.map((el) => (
-            <div key={el.id} id={el.id} className="p-6 bg-white rounded-md">
-              <img src={el.icon} alt="node.js" className="w-[60px] h-[60px]"/>
+            <div className="mb-12">
+              <div
+                key={el.id}
+                id={el.id}
+                className="p-6 bg-white rounded-md mb-2 flex flex-col"
+              >
+                <img
+                  src={el.icon}
+                  alt="node.js"
+                  className="w-[60px] h-[60px]"
+                />
+              </div>
+              <div className="text-[#c49b66] text-center font-semibold text-xl">
+                {el.text}
+              </div>
             </div>
           ))}
         </div>
